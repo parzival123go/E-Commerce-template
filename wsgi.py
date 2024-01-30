bbd9533b8e3892f5ffefb2fd709e3f2d3bc61ff6
littbrian <limonkim79@gmail.com>
2024-01-30 16:02:27 +0000

    fixed login page and added user

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 import click
 import csv
-from App import db, User, Pokemon, UserPokemon
+from tabulate import tabulate
+from App import db, User
 from App import app
+
 
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
@@ -10,20 +12,6 @@ def initialize():
   bob = User("bob", "bob@mail.com", "bobpass")
   db.session.add(bob)
   db.session.commit()
-  
-  with open('pokemon.csv', newline='', encoding='utf8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-      if row['height_m'] == '':
-        row['height_m'] = None
-      if row['weight_kg'] == '':
-        row['weight_kg'] = None
-      if row['type2'] == '':
-        row['type2'] = None
-
-      pokemon = Pokemon(name=row['name'], attack=row['attack'], defense=row['defense'], sp_attack=row['sp_attack'], sp_defense=row['sp_defense'], weight=row['weight_kg'], height=row['height_m'], hp=row['hp'], speed=row['speed'], type1=row['type1'], type2=row['type2'])
-      db.session.add(pokemon)
-    db.session.commit()
-  print('database initialized')
+  print("database Initialized")
 
   
