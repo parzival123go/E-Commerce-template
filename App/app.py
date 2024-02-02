81677eb227aebe02189ceaf6ce5faeea32952ef2
littbrian <limonkim79@gmail.com>
2024-02-02 20:07:31 +0000

    dashboard

--- App/app.py
+++ App/app.py
@@ -59,6 +59,10 @@ def signup():
   return render_template("signup.html")
 
 
+@app.route('/dashboard')
+def dashboard():
+  return render_template("dashboard.html")
+
 @app.route("/login", methods=['POST'])
 def login_func():
     data = request.form      #gets data entered by user (username, pw)
