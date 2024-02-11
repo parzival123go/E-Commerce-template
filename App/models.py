ad6a3ac323e9b638b8d6924e7b4916439c6f1f24
Sanjeev Praboocharan <sanjeevpraboo@gmail.com>
2024-02-11 08:03:58 +0000

    login/signup set up

--- App/models.py
+++ App/models.py
@@ -6,14 +6,14 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(80), unique=True, nullable=False)
+  name = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(120), nullable=False)
   is_admin = db.Column(db.Boolean, default=False)
 
 
-  def __init__(self, username, email, password):
-    self.username = username
+  def __init__(self, name, email, password):
+    self.name = name
     self.email = email
     self.set_password(password)
 
@@ -30,7 +30,7 @@ class User(db.Model, UserMixin):
   
   #To String method
   def __repr__(self):
-      return f'<User {self.id}: {self.username}>'  
+      return f'<User {self.id}: {self.name}>'  
     
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
